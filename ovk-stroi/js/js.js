9e86555168f29bc21c03a83fc5bfc6a149795afe
Allifer <Allifer@users.noreply.github.com>
2017-10-26 21:40:11 +0300

    commit ovk

---
+++ ovk-stroi/js/js.js
@@ -0,0 +1,24 @@
+
+   $('.sl').slick({
+	
+autoplay: true,
+autoplaySpeed: 2000,
+speed: 100,
+cssEase: 'ease-in',	
+
+centerPadding: '25px',
+dots: true,
+arrows: false,
+fade: true,	
+pauseOnDotsHover: true
+});
+	 
+    
+    
+    
+    
+    
+    
+    
+    
+    
